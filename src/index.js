19ba322bc4446b1a955dbd034de6a5000abbcfb2
Samuel Mwaura <mwaurakimemia08@gmail.com>
2026-04-27 02:47:22 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+   const taskForm=document.querySelector("#create-task-form");
+
+   taskForm.addEventListener("submit",(event) =>{
+    event.preventDefault();
+
+    const task=document.getElementById('new-task-description').value
+    buildToDo(task)
+})
+function buildToDo(task){
+  const li=document.createElement("li")
+  li.textContent=task
+  document.querySelector('#tasks').append(li)
+}
 });
+if (typeof module !== 'undefined') {
+  module.exports = { buildToDo }
+}
